06ae4a270c9678a1137e8cdd0441d2b080bb4cea
DongBoumShin <42655143+DongBoumShin@users.noreply.github.com>
2023-09-15 22:52:36 +0900

    Added content_scripts
    content_scripts.js according to convention

---
+++ Phishing_Box/content_scripts.js
@@ -0,0 +1,11 @@
+// 컨텐츠 스크립트 (content.js)
+
+// 세부 URL을 가져옴
+const detailURL = window.location.href;
+
+
+(async () => {
+  const response = await chrome.runtime.sendMessage({ action: 'checkURL', url: detailURL });
+  // do something with response here, not outside the function
+  console.log(response)
+})();
