b4628f7a3f475ad86a23c4d255aa2bd12d552cca
serimi <125631683+serimi@users.noreply.github.com>
2023-09-25 22:10:25 +0900

    0925

--- Phishing_Box/background.js
+++ Phishing_Box/background.js
@@ -1,43 +1,40 @@
-const predictionResults = {};
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  if (request.action === 'checkURL') {
+    const urlToCheck = request.url;
+    const rooturl = request.rootURL;
 
-function updateIcon(isMalicious) {
-  const iconPath = isMalicious ? "red.png" : "green.png";
-  chrome.browserAction.setIcon({ path: iconPath });
-}
+    chrome.storage.local.get(rooturl, function (result) {
+      if (Object.keys(result).length !== 0) {
+        console.log("Cached data found");
+        sendResponse({ results: result[rooturl] });
+      } else {
+        console.log("New data");
 
+        
+        fetch('http://15.164.49.80:8000/predict', {
+          method: 'POST',
+          body: JSON.stringify({ url: urlToCheck }),
+          headers: {
+            'Content-Type': 'application/json',
+            Accept: 'application/json',
+          },
+        })
+          .then((response) => response.json())
+          .then((result) => {
+            
+            const dataToCache = {};
+            dataToCache[rooturl] = result.predict_result;
+            chrome.storage.local.set(dataToCache);
 
-function getPrediction(urlToCheck, sendResponse) {
-  if (predictionResults[urlToCheck]) {
-    const isMalicious = predictionResults[urlToCheck] === 'malicious';
-    updateIcon(isMalicious);
-    sendResponse({ result: predictionResults[urlToCheck] });
-  } else {
-    fetch('http://15.164.49.80:8000/predict', {
-      method: 'POST',
-      body: JSON.stringify({ url: urlToCheck }),
-      headers: {
-        'Content-Type': 'application/json'
+            sendResponse({ results: result.predict_result });
+          })
+          .catch((error) => {
+            console.error('데이터를 가져오는 중 오류 발생:', error);
+            sendResponse({ results: "SERVER ERROR" });
+          });
       }
-    })
-      .then(response => response.json())
-      .then(result => {
-        const isMalicious = result.status === 'malicious';
-        predictionResults[urlToCheck] = result.status; 
-        updateIcon(isMalicious); 
-        sendResponse({ result: result.status });
-      })
-      .catch(error => {
-        console.error('error', error);
-        alert('error');
-        sendResponse({ result: 'error' });
-      });
-  }
-}
+    });
 
-chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
-  if (message.action === 'checkURL') {
-    const urlToCheck = message.url;
-    getPrediction(urlToCheck, sendResponse);
-    return true; 
+    return true;
   }
 });
