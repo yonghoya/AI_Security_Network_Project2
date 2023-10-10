c4e340ed68b6dcd6d1745b6630d44b407533b019
YongHo Kim <39542937+yonghoya@users.noreply.github.com>
2023-10-10 19:30:17 +0900

    크롬 확장팩 파일 추가
    background.js, content.js, input.html, popup.html 추가

--- Phishing Box/content.js
+++ Phishing Box/content.js
@@ -1,64 +1,12 @@
-// content.js
-const allowedWebsites = ["https://www.naver.com/", "https://www.google.com/"]; // 허용할 웹사이트 목록
+var element = document.querySelector('div.feedmain');
+element.parentElement.removeChild(element);
+// 컨텐츠 스크립트 (content.js)
 
-const currentURL = window.location.href;
+// 세부 URL을 가져옴
+const detailURL = window.location.href;
 
-if (allowedWebsites.includes(currentURL)) {
-  
-  const allowedDiv = createDraggableCard('green', `Allowed: ${currentURL}`);
-  document.body.appendChild(allowedDiv);
-} else {
-  
-  const blockedDiv = createDraggableCard('red', `Blocked: ${currentURL}`);
-  document.body.appendChild(blockedDiv);
-}
-
-// 드래그 가능한 카드를 생성하는 함수
-function createDraggableCard(backgroundColor, text) {
-  const cardDiv = document.createElement('div');
-  cardDiv.style.position = 'fixed';
-  cardDiv.style.top = '20px';
-  cardDiv.style.right = '20px';
-  cardDiv.style.backgroundColor = backgroundColor;
-  cardDiv.style.padding = '20px';
-  cardDiv.style.boxShadow = '0px 0px 10px rgba(0, 0, 0, 0.2)';
-  cardDiv.style.borderRadius = '5px';
-  cardDiv.style.zIndex = '9999';
-  cardDiv.style.overflow = 'hidden';
-  cardDiv.style.textOverflow = 'ellipsis';
-  cardDiv.style.whiteSpace = 'nowrap';
-  cardDiv.style.cursor = 'grab';
-
-  const cardText = document.createElement('div');
-  cardText.textContent = text;
-
-  cardDiv.appendChild(cardText);
-
-  let isDragging = false;
-  let offsetX, offsetY;
-
-  cardDiv.addEventListener('mousedown', (e) => {
-    isDragging = true;
-    offsetX = e.clientX - cardDiv.getBoundingClientRect().left;
-    offsetY = e.clientY - cardDiv.getBoundingClientRect().top;
-    cardDiv.style.cursor = 'grabbing';
-  });
-
-  document.addEventListener('mousemove', (e) => {
-    if (!isDragging) return;
-
-    const newX = e.clientX - offsetX;
-    const newY = e.clientY - offsetY;
-
-    cardDiv.style.right = 'auto';
-    cardDiv.style.top = newY + 'px';
-    cardDiv.style.left = newX + 'px';
-  });
-
-  document.addEventListener('mouseup', () => {
-    isDragging = false;
-    cardDiv.style.cursor = 'grab';
-  });
-
-  return cardDiv;
-}
+// 백그라운드 스크립트에 메시지를 보냄
+chrome.runtime.sendMessage({ action: 'checkURL', url: detailURL }, function(response) {
+  // 백그라운드 스크립트에서 응답이 올 경우 처리할 로직을 추가할 수 있음
+  alert(response);
+});
